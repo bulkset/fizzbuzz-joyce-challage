da2d126af133252e05126d25136f5f753d6fffc6
ScaryTools <chupapa327@rambler.ru>
2023-10-25 14:13:50 +0500

    optimization

--- main.c
+++ main.c
@@ -1,83 +1,33 @@
 #include <stdio.h>
 
-void funcFizzBuzz(int num1, int num2);
-
-int main(void){
-    int num1;
-    int num2;
-
-    printf("Please enter the first number: ");
+int main() {
+    int num1, num2;
+    
+    printf("Input number 1: ");
     scanf("%d", &num1);
-    printf("Please enter the second number: ");
+    printf("Input number 2: ");
     scanf("%d", &num2);
-
-    funcFizzBuzz(num1, num2);
-
-    return 0;
-}
-
-void funcFizzBuzz(int num1, int num2){
-    int results[num2 - num1 + 1];
-    int i = 0;
-    if(num2 > num1){
-        for(;num1 <= num2; num1++){
-
-            if(num1 % 3 == 0 && num1 % 5 == 0){
-                results[i] = -1; // FizzBuzz
-                i++;
-            }else if(num1 % 3 == 0){
-                results[i] = -2; // Fizz
-                i++;
-            }else if(num1 % 5 == 0){
-                results[i] = -3; // Buzz
-                i++;
-            }else{
-                results[i] = num1;
-                i++;
-            }
-
-
+    
+    int start = (num1 < num2) ? num1 : num2;
+    int end = (num1 < num2) ? num2 : num1;
+    
+    for (int i = start; i <= end; i++) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            printf("FizzBuzz");
+        } else if (i % 3 == 0) {
+            printf("Fizz");
+        } else if (i % 5 == 0) {
+            printf("Buzz");
+        } else {
+            printf("%d", i);
         }
-        for(int j = 0; j < i; j++){
-            if(results[j] == -1){
-                printf("FizzBuzz ");
-            }else if(results[j] == -2){
-                printf("Fizz ");
-            }else if(results[j] == -3){
-                printf("Buzz ");
-            }else{
-                printf("%d ", results[j]);
-            }
-        }
-    }else{
-        for(;num2 <= num1; num2++){
-
-            if(num2 % 3 == 0 && num2 % 5 == 0){
-                results[i] = -1; // FizzBuzz
-                i++;
-            }else if(num2 % 3 == 0){
-                results[i] = -2; // Fizz
-                i++;
-            }else if(num2 % 5 == 0){
-                results[i] = -3; // Buzz
-                i++;
-            }else{
-                results[i] = num2;
-                i++;
-            }
-
-
-        }
-        for(int j = 0; j < i; j++){
-            if(results[j] == -1){
-                printf("FizzBuzz ");
-            }else if(results[j] == -2){
-                printf("Fizz ");
-            }else if(results[j] == -3){
-                printf("Buzz ");
-            }else{
-                printf("%d ", results[j]);
-            }
+        
+        if (i < end) {
+            printf(", ");
         }
     }
+    
+    printf("\n");
+
+    return 0;
 }
